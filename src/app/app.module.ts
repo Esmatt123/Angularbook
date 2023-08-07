61a44e1b3a224a3ee6dd68565bc049188e912686
Esmatt Morra <esmatt98@hotmail.com>
2023-08-07 16:18:12 +0200

    Börjat med front end delen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginModalComponent } from './auth/modals/login-modal/login-modal.component';
+import { RegisterModalComponent } from './auth/modals/register-modal/register-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LoginModalComponent,
+    RegisterModalComponent
   ],
   imports: [
     BrowserModule,
